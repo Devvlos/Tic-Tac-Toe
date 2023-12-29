2f5dd6e4b81178d6d1822fefa46857631e34f4a3
Devvlos <devcantcode@gmail.com>
2023-12-29 21:55:23 +0530

    file added

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Tic-Tac-Toe
-Tic-tac-toe, noughts and crosses, or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner
+<h1 align="center">Tic-Tac-Toe</h1>
+<h3 align="center">Tic-tac-toe, noughts and crosses, or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner</h3>
+
+
+[![Repository Status](https://img.shields.io/badge/Repository%20Status-Maintained-dark%20green.svg)](https://github.com/Devvlos/Tic-Tac-Toe)
+[![Website Status](https://img.shields.io/badge/Website%20Status-Online-green)](https://devvlos.github.io/Tic-Tac-Toe/)
+[![Author](https://img.shields.io/badge/Author-Adhish%20Gupta-blue.svg)](https://adhishgupta.rf.gd/)
+[![Latest Release](https://img.shields.io/badge/Latest%29Release-20%20December%202023-yellow.svg)](https://github.com/Devvlos/Tic-Tac-Toe)
+
+
+<p align="center">
+  <a href="#-about-the-project">About the project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-getting-started">Getting started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-contribute">How to contribute</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+---
+
+<p align="center">
+  <img alt="screenshot" src="demo.png">
+</p>
+
+---
+
+## 💡 About the project
+
+This is an open source project that serves as a free alternative to the Linktree website.
+- [Demo](https://devvlos.github.io/Tic-Tac-Toe/)
+
+## 🚀 Getting started
+
+If you installed git you can clone the code to your machine, or download a ZIP of all the files directly.
+[Download the ZIP from this location](https://github.com/Devvlos/Tic-Tac-Toe/archive/refs/heads/main.zip), or run the following [git](https://git-scm.com/downloads) command to clone the files to your machine:
+```bash
+git clone https://github.com/Devvlos/Tic-Tac-Toe.git
+```
+- Once the files are on your machine, open the _linktree_ folder in [Visual Studio Code](https://code.visualstudio.com/).
+- With the files open in Visual Studio Code, press the **Go Live** button at the bottom of the window to launch the files with [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer).
+
+## 🤔 How to contribute
+
+- Fork this repository;
+- Create a branch with your feature: `git checkout -b my-feature`;
+- Commit your changes: `git commit -m "feat: my new feature"`;
+- Push to your branch: `git push origin my-feature`.
+
+Once your pull request has been merged, you can delete your branch.
+
+
+Made By Devlos :wave: [Contact](http://adhishgupta.rf.gd/)
---
+++ main.js
@@ -0,0 +1,123 @@
+let result = document.getElementById("Result");
+let boxes =  document.querySelectorAll(".box");
+let resetBtn = document.getElementById("reset");
+
+
+
+let count = 0;
+
+
+
+let playerPlaying = "x";
+let gameComplete = false;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+]
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (gameComplete === false)
+        {
+            count++;
+            if (playerPlaying === "x")
+            {
+                box.innerHTML = "X"
+                box.style.color = "red"
+                playerPlaying = "o"
+            }
+            else
+            {
+                box.innerHTML = "O"
+                box.style.color = "green"
+                playerPlaying = "x"
+            }
+            box.disabled = true;
+            checkWinner();
+        }
+    })
+})
+
+let pointX = 0;
+let pointO = 0;
+const checkWinner = () => {
+    for(let pattern of winPatterns)
+    {
+        pl1 = document.getElementById('Player1').value;
+        pl2 = document.getElementById('Player2').value;
+
+        if (boxes[pattern[0]].innerHTML === "X" && boxes[pattern[1]].innerHTML === "X" && boxes[pattern[2]].innerHTML === "X")
+        {
+            result.innerHTML = 'Congratulations ' + pl1 + ' Won';
+            result.style.opacity = "1";
+            gameComplete = true;
+            pointX++;
+            document.getElementById("Playerinfo1").innerHTML = pl1 + ' : ' + pointX;
+        }
+        if (boxes[pattern[0]].innerHTML === "O" && boxes[pattern[1]].innerHTML === "O" && boxes[pattern[2]].innerHTML === "O")
+        {
+            result.innerHTML = 'Congratulations ' + pl2 + ' Won';
+            result.style.opacity = "1";
+            gameComplete = true;
+            pointO++;
+            document.getElementById("Playerinfo2").innerHTML = pointO + ' : ' + pl2;
+        }
+    }
+    if (count >= 9 && gameComplete === false)
+    {
+       
+       result.innerHTML = "Game Tie Nerds";
+       result.style.opacity = "1";
+    }
+}
+const reset = () => 
+{
+    count = 0;
+    gameComplete = false;
+    result.style.opacity = "0";
+    for(i = 0; i < 9; i++)
+    {
+        boxes[i].innerHTML = "";
+        boxes[i].disabled = false;
+    }
+}
+
+resetBtn.addEventListener("click", reset);
+
+
+const shareBtn = document.getElementById('shareBtn')
+
+shareBtn.addEventListener('click', event => {
+
+  // Check for Web Share api support
+  if (navigator.share) {
+    // Browser supports native share api
+    navigator.share({
+      text: 'Please read this great article: ',
+      url: 'https://www.google.com/',
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+      .catch((err) => console.error(err));
+  } else {
+    // Fallback
+    alert("The current browser does not support the share function. Please, manually share the link")
+  }
+});
+
+
+function playerName()
+{
+    var player1 = document.getElementById('Player1').value;
+    var player2 = document.getElementById('Player2').value;
+    document.getElementById('InputPlayer').style.display = "none";
+    document.getElementById("Playerinfo1").innerHTML = player1 + ' : 0';
+    document.getElementById("Playerinfo2").innerHTML = '0 :  ' + player2;
+}
